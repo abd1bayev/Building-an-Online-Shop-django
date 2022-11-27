3b23a2464f204b7b9797d6508f8deb8dcfdbe34f
jasur <jasurabdibayev0@gmail.com>
2022-11-27 14:53:57 +0500

    all

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    lactation = models.CharField(max_length=30)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    adult = models.IntegerField()
+    children = models.IntegerField()
+    room = models.IntegerField()
+
+    def __str__(self):
+        return self.lactation
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
